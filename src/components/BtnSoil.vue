847885e925a99e271a0b050ad25d81be2df8f152
Exceil Matthew Carpio <117639310+Exceil-Carpio@users.noreply.github.com>
2023-05-27 19:21:33 +0800

    Add files via upload

---
+++ src/components/BtnSoil.vue
@@ -0,0 +1,98 @@
+<template>
+    <div>
+        <button :class="[type, disabled ? 'disabled' : '']" :disabled="disabled">
+            <p>{{ text }}</p>
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'btnSoil',
+        data(){
+            return{
+
+            }
+        },
+        props: {
+            text: String,
+            disabled: {
+                type: Boolean,
+                default: false
+            },
+            type: {
+                type: String,
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    button{
+        background: #B08968;
+        color: #E6CCB2;
+        padding: 10px;
+        border: none;
+        box-shadow: 2px 2px rgb(0, 0, 0, 0.5);
+    }
+    
+    button:hover{
+        background: #7F5539;
+        color: #EDE0D4;
+        box-shadow: 3px 3px rgb(0, 0, 0, 0.5);
+    }
+
+    .disabled{
+        pointer-events: none;
+    }
+
+    .disabled:hover{
+        pointer-events: none;
+    }
+
+    .outlined{
+        border: 1px solid black;
+        box-shadow: none;
+    }
+
+    .outlined:hover{
+        border: 1px solid black;
+        box-shadow: none;
+    }
+
+    .text{
+        background: none;
+        box-shadow: none;
+        color: #7F5539;
+    }
+
+    .text:hover{
+        box-shadow: none;
+        color: #7F5539;
+        background: #EDE0D4;
+    }
+
+    .tonal{
+        box-shadow: none;
+        color: #392415;
+        background: #E6CCB2;
+    }
+
+    .tonal:hover{
+        background: #B08968;
+        color: #392415;
+        box-shadow: none;
+    }
+
+    .plain{
+        background: none;
+        box-shadow: none;
+        color: #B08968;
+    }
+
+    .plain:hover{
+        background: none;
+        box-shadow: none;
+        color: #392415;
+    }
+</style>
