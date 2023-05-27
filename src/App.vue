854a5265a2a34e8683c40ba201249a95f9cf977d
Exceil-Carpio <exceilmatthew.carpio@gmail.com>
2023-05-27 15:07:22 +0800

    resources

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default{
+  name: 'App',
+  components: {
+    HelloWorld,
+  }
+}
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="Just a Sample Program" />
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <h3>{{ msg }}</h3>
+</template>
+
+<style scoped>
+h3 {
+  font-size: 1.2rem;
+}
+</style>
