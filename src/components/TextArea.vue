584e1a19e0839993f10cd43112873112b3da27f1
cris <130618382+crissopogi@users.noreply.github.com>
2023-05-28 23:44:19 +0800

    Add files via upload

---
+++ src/components/TextArea.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="text-area">
+    <textarea v-model="text" class="textarea"></textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: 'This is a textbox'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.text-area {
+  width: 300px;
+}
+
+.textarea {
+  width: 100%;
+  height: 150px;
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  resize: vertical;
+  background-color: #7F5539;
+  color: white;
+}
+</style>
