d9f255bec4079c9018e89ba41f282203e5425827
Exceil Matthew Carpio <117639310+Exceil-Carpio@users.noreply.github.com>
2023-05-28 13:30:19 +0800

    Update BtnSoil.vue
    Designs when buttons is disabled, and click effect on each button types

--- src/components/BtnSoil.vue
+++ src/components/BtnSoil.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <button :class="[type, disabled ? 'disabled' : '']" :disabled="disabled">
+        <button :class="[type, disabled ? 'disabled' : '']" :disabled="disabled" id="sizes">
             <p>{{ text }}</p>
         </button>
     </div>
@@ -9,11 +9,6 @@
 <script>
     export default {
         name: 'btnSoil',
-        data(){
-            return{
-
-            }
-        },
         props: {
             text: String,
             disabled: {
@@ -42,6 +37,19 @@
         box-shadow: 3px 3px rgb(0, 0, 0, 0.5);
     }
 
+    button:active{
+        background: #B08968;
+        color: #E6CCB2;
+        box-shadow: 2px 2px rgb(0, 0, 0, 0.5);
+        transition: 0s;
+    }
+
+    button:disabled,
+    button[disabled]{
+        background: #E6CCB2;
+        color: #EDE0D4;
+    }
+
     .disabled{
         pointer-events: none;
     }
@@ -60,6 +68,20 @@
         box-shadow: none;
     }
 
+    .outlined:active{
+        border: 1px solid black;
+        background: #B08968;
+        color: #E6CCB2;
+        box-shadow: none;
+        transition: 0s;
+    }
+
+    .outlined:disabled,
+    .outlined[disabled]{
+        background: #E6CCB2;
+        color: #EDE0D4;
+    }
+
     .text{
         background: none;
         box-shadow: none;
@@ -70,6 +92,19 @@
         box-shadow: none;
         color: #7F5539;
         background: #EDE0D4;
+    }
+
+    .text:active{
+        background: none;
+        box-shadow: none;
+        color: #7F5539;
+        transition: 0s;
+    }
+
+    .text:disabled,
+    .text[disabled]{
+        color: #E6CCB2;
+        background: none;
     }
 
     .tonal{
@@ -84,6 +119,19 @@
         box-shadow: none;
     }
 
+    .tonal:active{
+        box-shadow: none;
+        color: #392415;
+        background: #E6CCB2;
+        transition: 0s;
+    }
+
+    .tonal:disabled,
+    .tonal[disabled]{
+        background: #E6CCB2;
+        color: #EDE0D4;
+    }
+
     .plain{
         background: none;
         box-shadow: none;
@@ -94,5 +142,18 @@
         background: none;
         box-shadow: none;
         color: #392415;
+    }
+
+    .plain:active{
+        background: none;
+        box-shadow: none;
+        color: #B08968;
+        transition: 0s;
+    }
+
+    .plain:disabled,
+    .plain[disabled]{
+        color: #E6CCB2;
+        background: none;
     }
 </style>
