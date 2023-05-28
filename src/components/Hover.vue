3b98bdb2ecf24be0eab27e60c0c429ba339ac8c5
Rina Mariz Paler <130961407+RinaPaler@users.noreply.github.com>
2023-05-28 19:37:52 +0800

    Add files via upload

---
+++ src/components/Hover.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="Hover">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hover',
+  props: {
+    type: String,
+  },
+}
+</script>
+
+<style scoped>
+.Hover {
+  cursor: pointer;
+  font-size: 20px;
+}
+
+.Hover:hover {
+  background-color: #EDE0D4;
+  color: #7F5539;
+}
+</style>
