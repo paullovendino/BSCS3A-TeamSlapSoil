37d7cfa1b09ce62b44921f4c101cba1ed9ba2a44
naomipxdillx <131147361+naomipxdillx@users.noreply.github.com>
2023-05-28 23:20:21 +0800

    Add files via upload

---
+++ src/components/Image.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="image-component-wrapper">
+    <div class="image-wrapper">
+        <img :src="imageUrl" :alt="altText" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    imageUrl: {
+      type: String,
+      required: true
+    },
+    altText: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+    .image-component-wrapper{
+        display: flex;
+    }
+    .image-wrapper{
+        display: flex;
+        height: 300px;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
