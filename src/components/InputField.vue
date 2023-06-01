b07d54a63194f1faf3f8f4a7e6f3fe847fa6ab59
pieyeyjey <130520195+pieyeyjey@users.noreply.github.com>
2023-06-02 00:12:17 +0800

    Add files via upload

---
+++ src/components/InputField.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <input type="text" :class="[inputStyle, inputClass]" v-model="inputText" placeholder="Enter text">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    inputStyle: {
+      type: String,
+      default: "input-field"
+    }
+  },
+  data() {
+    return {
+      inputText: '',
+    };
+  },
+  computed: {
+    inputClass() {
+      return this.inputStyle;
+    }
+  }
+};
+</script>
+
+<style>
+.input-field {
+  background-color: #E6CCB2;
+  padding: 10px;
+  border: 1px solid #7F5539;
+  border-radius: 4px;
+  outline: #7F5539;
+}
+
+.underlined {
+  border: none;
+  border-bottom: 1px solid #7F5539;
+  outline: none;
+}
+
+.outlined {
+  outline: 1px solid #7F5539;
+}
+
+.solo {
+  background-color: transparent;
+  border: none;
+  outline: none;
+}
+
+.solo-filled {
+  background-color: #E6CCB2;
+  border: none;
+  outline: none;
+}
+
+.solo-inverted {
+  background-color: #7F5539;
+  color: #E6CCB2;
+  border: none;
+  outline: none;
+}
+</style>
