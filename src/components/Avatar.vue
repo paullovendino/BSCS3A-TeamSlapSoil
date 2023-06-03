1543f787ac27a7f79c9a50f0b8e54c2efd26adb0
Rina Mariz Paler <130961407+RinaPaler@users.noreply.github.com>
2023-06-03 22:10:39 +0800

    Add files via upload

---
+++ src/components/Avatar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="avatar">
+    <img :src="imageUrl" :alt="altText" class="avatar-image">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    imageUrl: {
+      type: String,
+      required: true
+    },
+    altText: {
+      type: String,
+      default: 'Avatar'
+    }
+  }
+};
+</script>
+
+<style scoped>
+.avatar {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  overflow: hidden;
+  background-color: #E6CCB2;
+}
+
+.avatar-image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
