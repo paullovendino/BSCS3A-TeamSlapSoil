c6a4f8616dd95d8d68d8698062b69947f11634ff
cris <130618382+crissopogi@users.noreply.github.com>
2023-06-03 18:44:49 +0800

    ComboBox.vue

--- src/components/ComboBox.vue
+++ src/components/ComboBox.vue
@@ -1,44 +1,55 @@
 <template>
-    <div class="combo-box">
-      <select v-model="selectedOption" class="select-box">
-        <option v-for="option in options" :key="option.value" :value="option.value">{{ option.label }}</option>
-      </select>
-      <span class="selected-option">Selected option: {{ selectedOption }}</span>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        selectedOption: '',
-        options: [
-          { value: 'London', label: 'London' },
-          { value: 'Paris', label: 'Paris' },
-          { value: 'Milan', label: 'Milan' }
-        ]
-      };
-    }
-  };
-  </script>
-  
-  <style scoped>
-  .combo-box {
-    display: flex;
-    align-items: center;
+  <div class="combo-box">
+    <select v-model="selectedOption" class="select-box">
+      <option v-for="option in options" :key="option.value" :value="option.value">{{ option.label }}</option>
+    </select>
+    <span class="selected-option">Selected option: {{ selectedOption }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedOption: '',
+      options: [
+        { value: 'London', label: 'London' },
+        { value: 'Paris', label: 'Paris' },
+        { value: 'Milan', label: 'Milan' }
+      ]
+    };
   }
-  
-  .select-box {
-    padding: 8px;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    background-color: #9C6644;
-    color: white;
-  }
-  
-  .selected-option {
-    margin-left: 8px;
-    color: brown;
-  }
-  </style>
-  
+};
+</script>
+
+<style scoped>
+.combo-box {
+  display: flex;
+  align-items: center;
+}
+
+.select-box {
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background-color: #9C6644;
+  color: white;
+  font-family: Arial, sans-serif;
+  font-size: 20px;
+  line-height: 1.5;
+  cursor: pointer;
+  transition: background-color 0.3s ease-in-out;
+  width: 200px;
+}
+
+.select-box:hover {
+  background-color: #7F5539;
+}
+
+.selected-option {
+  margin-left: 8px;
+  color: #9C6644;
+  font-size: 16px;
+  font-weight: bold;
+}
+</style>
