5bc487444a367c85011e1ca8df99b322818546a2
cris <130618382+crissopogi@users.noreply.github.com>
2023-06-03 18:33:06 +0800

    Add files via upload

---
+++ src/components/ComboBox.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="combo-box">
+      <select v-model="selectedOption" class="select-box">
+        <option v-for="option in options" :key="option.value" :value="option.value">{{ option.label }}</option>
+      </select>
+      <span class="selected-option">Selected option: {{ selectedOption }}</span>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        selectedOption: '',
+        options: [
+          { value: 'London', label: 'London' },
+          { value: 'Paris', label: 'Paris' },
+          { value: 'Milan', label: 'Milan' }
+        ]
+      };
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .combo-box {
+    display: flex;
+    align-items: center;
+  }
+  
+  .select-box {
+    padding: 8px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    background-color: #9C6644;
+    color: white;
+  }
+  
+  .selected-option {
+    margin-left: 8px;
+    color: brown;
+  }
+  </style>
+  
