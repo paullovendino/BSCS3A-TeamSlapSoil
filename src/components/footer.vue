a1ae2dd317eaee4b026f4603a91ce1f3fb53dbb7
RUSSEL, LESTER JHON F <130618085+LesterRRRRRRR@users.noreply.github.com>
2023-06-04 07:29:09 -0800

    Add files via upload

---
+++ src/components/footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <p>&copy; {{ currentYear }}</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentYear: 'All Rights Reserved 2023',
+    };
+  },
+};
+</script>
+
+<style>
+.footer {
+  background-color: #d4a373;
+  padding: 15px;
+  text-transform: capitalize;
+  text-align: center;
+  font-size: 20px;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  color: #780116;
+}
+</style>
