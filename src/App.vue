481c5c93aa5ef419fefc3d0e83146e7b086ed193
Exceil Matthew Carpio <117639310+Exceil-Carpio@users.noreply.github.com>
2023-06-05 13:11:32 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,35 @@
+<template>
+  <TabbedNavigation webTitle="Slap Soil" sidebarTitle="Components">
+    <Tabs title="Component 1"></Tabs>
+    <Tabs title="Component 2"></Tabs>
+    <Tabs title="Component 3"></Tabs>
+    <Tabs title="Component 4"></Tabs>
+    <Tabs title="Component 5"></Tabs>
+    <Tabs title="Component 6"></Tabs>
+    <Tabs title="Component 7"></Tabs>
+    <Tabs title="Component 8"></Tabs>
+    <Tabs title="Component 9"></Tabs>
+  </TabbedNavigation>
+</template>
+
 <script>
-import MySwitch from './components/MySwitch.vue'
-import Image from './components/Image.vue'
+import Tabs from './components/tabs.vue'
+import TabbedNavigation from './components/TabbedNavigation.vue'
 
 export default{
   name: 'App',
   components: {
-    MySwitch,
-    Image
+    Tabs,
+    TabbedNavigation,
   },
   data (){
     return{
-      switchValue: false
+      
     }
   }
 }
 </script>
 
-<template>
-  <header>
-    <div class="wrapper">
-      <MySwitch :switch-value="switchValue" @switchValueChanged="switchValue = !switchValue"></MySwitch>
-      <Image :imageUrl="'https://www.positivethinking.tech/wp-content/uploads/2021/01/Logo-Vuejs.png'"></Image>
-    </div>
-  </header>
-</template>
-
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-}
+  
 </style>
