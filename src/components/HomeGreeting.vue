a4c6a5a24b27d5987fba271b2523274468133d0f
paullovendino <106613288+paullovendino@users.noreply.github.com>
2023-06-06 16:57:47 +0800

    Add files via upload

---
+++ src/components/HomeGreeting.vue
@@ -0,0 +1,22 @@
+<template>
+        <div class="greetings">
+            <h1>{{ greeting }}</h1>
+        </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return{
+            greeting: "WELCOME"
+        }
+    }
+}
+</script>
+
+<style>
+    .greetings h1{
+        color: white;
+        font-size: 140px;
+    }
+</style>
