da134e22440bf2bea85c374f40c22f70d1d9568c
paullovendino <106613288+paullovendino@users.noreply.github.com>
2023-06-06 17:08:03 +0800

    Update HomeGreeting.vue

--- src/components/HomeGreeting.vue
+++ src/components/HomeGreeting.vue
@@ -2,10 +2,17 @@
         <div class="greetings">
             <h1>{{ greeting }}</h1>
         </div>
+        <btnSoil text="Start Browsing" />
 </template>
 
+
 <script>
+import btnSoil from './btnSoil.vue'
+
 export default {
+    components: {
+        btnSoil
+    },
     data () {
         return{
             greeting: "WELCOME"
