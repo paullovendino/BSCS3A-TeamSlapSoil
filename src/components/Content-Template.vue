ce84e38c5330aa49e435b523c466983ad9eddb3a
paullovendino <106613288+paullovendino@users.noreply.github.com>
2023-06-08 00:28:54 +0800

    Add files via upload

---
+++ src/components/Content-Template.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="content-container">
+        <div class="borderline">
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+    .content-container{
+        height: 75vh;
+        width: 160vh;
+        background-color: #ede0d4;
+        opacity: .9;
+        border-radius: 50px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .borderline{
+        height: 72vh;
+        width: 156vh;
+        background: transparent;
+        border: #CCA481 solid 7px;
+        border-radius: 50px;
+    }
+</style>
