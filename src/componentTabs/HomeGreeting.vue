0046a59ac3fd488a9adb9401133269843e08844a
Exceil Matthew Carpio <117639310+Exceil-Carpio@users.noreply.github.com>
2023-06-11 17:05:18 +0800

    Add files via upload

---
+++ src/componentTabs/HomeGreeting.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <div class="greetings">
+            <h1>{{ greeting }}</h1>
+        </div>
+        <btnSoil text="Start Browsing" />
+    </div>
+</template>
+
+
+<script>    
+import btnSoil from '/src/components/BtnSoil.vue'
+
+export default {
+    components: {
+        btnSoil
+    },
+    data () {
+        return{
+            greeting: "WELCOME"
+        }
+    }
+}
+</script>
+
+<style>
+    .greetings h1{
+        color: white;
+        font-size: clamp(6.25rem, 3.125rem + 10vw, 8.75rem);
+    }
+
+    .container{
+        position: absolute;
+        top: 50%;
+        transform: translate(0, -50%);
+        height: 90%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
