a841f27c105fb93bfb23a4705f48451c2750cbfd
Exceil Matthew Carpio <117639310+Exceil-Carpio@users.noreply.github.com>
2023-06-11 19:48:06 +0800

    Update and rename vertLine.vue to Line.vue

---
+++ src/components/Line.vue
@@ -0,0 +1,27 @@
+<template>
+    <div :class="alignment">
+    </div>
+</template>
+
+<script>
+    export default{
+        name:"Line",
+        props: {
+            alignment: String,
+        }
+    }
+</script>
+
+<style scoped>
+    div{
+        width: 100%;
+        height: 1px;
+        background-color: #7F5539;
+    }
+
+    .horizontal{
+        background-color: #7F5539;
+        height: 100%;
+        width: 1px;
+    }
+</style>
