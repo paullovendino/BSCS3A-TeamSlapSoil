7a9701e19efbde620ed012b990411bc9f91a627c
Exceil Matthew Carpio <117639310+Exceil-Carpio@users.noreply.github.com>
2023-06-11 18:36:25 +0800

    Update Checkbox.vue

--- src/components/Checkbox.vue
+++ src/components/Checkbox.vue
@@ -1,67 +1,45 @@
 <template>
   <div>
-    <input
-      type="checkbox" :name="name" :value="value" :id="id" :checked="checked" :disabled="disabled" @change="updateStatus" :ref="disabled ? 'disabledButton' : ''"/>
-    <label class="checkbox-label" :for="id">
-      <span class="checkbox-icon"></span>
-      {{ label }}
-    </label>
-  </div>
+        <input type="checkbox" :name="name" :value="value" :id="id" @change="$emit('isChecked', $event.target.checked)" />
+        <label class="checkbox-label" :for="name">
+            {{ label }}
+        </label>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'Checkbox',
-  props: {
-    name: String,
-    value: String,
-    label: String,
-    id: String,
-    checked: {
-      type: Boolean,
-      default: false
-    },
-    disabled: {
-      type: Boolean,
-      default: true
-    }
-  },
-  methods: {
-    updateStatus(event) {
-      const isChecked = event.target.checked;
-      this.$emit('update:checked', isChecked);
+  export default {
+    name: 'Checkbox',
+    props: {
+        name: String,
+        value: String,
+        label: String,
+        id: String,
     }
   }
-};
 </script>
 
-
 <style scoped>
+div{
+  display: flex;
+  align-items: center;
+}
+
 .checkbox-label {
   display: flex;
   align-items: center;
-  font-size: 20px;
-  margin-bottom: 8px;
-  color: #9C6644;
-}
-
-.checkbox-icon {
-  display: inline-block;
-  width: 16px;
-  height: 16px;
-  border: 2px solid brown;
-  border-radius: 4px;
-  position: relative;
-  top: 2px;
-  margin-right: 4px;
+  font-size: 15px;
+  color: #7F5539;
 }
 
 input[type="checkbox"] {
   appearance: none;
   -webkit-appearance: none;
   -moz-appearance: none;
-  width: 16px;
-  height: 16px;
+  width: 14px;
+  height: 14px;
+  border: 2px solid brown;
+  border-radius: 4px;
   outline: none;
   vertical-align: middle;
   position: relative;
@@ -69,12 +47,12 @@ input[type="checkbox"] {
   cursor: pointer;
 }
 
-input[type="checkbox"]:checked + .checkbox-label .checkbox-icon {
+input[type="checkbox"]:checked {
   background-color: brown;
   border-color: #DDB892;
 }
 
-input[type="checkbox"]:checked + .checkbox-label .checkbox-icon::after {
+input[type="checkbox"]:checked::after {
   content: "\2713";
   display: block;
   position: absolute;
@@ -83,14 +61,5 @@ input[type="checkbox"]:checked + .checkbox-label .checkbox-icon::after {
   transform: translate(-50%, -50%);
   font-size: 12px;
   color: white;
-}
-
-input[type="checkbox"]:disabled + .checkbox-label .checkbox-icon {
-  background-color: #E6CCB2;
-  border-color: #E6CCB2;
-}
-
-input[type="checkbox"]:disabled + .checkbox-label {
-  color: #E6CCB2;
 }
 </style>
