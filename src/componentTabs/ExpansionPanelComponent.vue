8b720c6a40511ee138c33f39e65f5dfd31302f8e
Exceil Matthew Carpio <117639310+Exceil-Carpio@users.noreply.github.com>
2023-06-12 14:26:04 +0800

    Update ExpansionPanelComponent.vue

--- src/componentTabs/ExpansionPanelComponent.vue
+++ src/componentTabs/ExpansionPanelComponent.vue
@@ -1,125 +1,82 @@
 <template>
-    <Template>
-        <div class="wrapperLeft">
-            <div class="wrapperTop">
-                <h1>Expansion Panel Component</h1>
-            </div>
-            <div class="wrapperBottom">
-                <ExpansionPanel title="This is a Sample Expansion Panel" :disabled="Disabled">
-                    <p>Sample Content Here</p>
-                </ExpansionPanel>
-            </div>
-        </div>
-
-        <div class="wrapperRight">
-            <h2>Attributes</h2>
-            <div class="attributes">
-                <table>
-                <tr>
-                    <td>
-                        <Checkbox label="Show Title" name="text" @isChecked="isDisabled"/>
-                    </td>
-                </tr>
-                </table>
-            </div>
-
-            <ExpansionPanel title="Types">
-                <table class="expandTable">
-                <tr>
-                    <td><RadioButton label="Option 1" name="switchType" value="" @valueChanged="TypeChanged" checked="true"/></td>
-                    <td><RadioButton label="Option 2" name="switchType" value="" @valueChanged="TypeChanged"/></td>
-                </tr>
-                </table>
-            </ExpansionPanel>
-            
-        </div> 
-    </Template>
+  <div class="expansion-panel">
+    <div class="panel-header" @click="togglePanel">
+      <span class="title">{{ title }}</span>
+      <span class="arrow-icon">{{ expanded ? '▼' : '►' }}</span>
+    </div>
+    <transition name="panel-transition">
+      <div v-show="expanded" class="panel-content">
+        <slot></slot>
+      </div>
+    </transition>
+  </div>
 </template>
 
 <script>
-import Template from '/src/components/Content-Template.vue'
-import RadioButton from '/src/components/RadioButton.vue'
-import Switch from '/src/components/MySwitchFinal.vue'
-import ExpansionPanel from '/src/components/ExpansionPanel.vue'
-import Checkbox from '/src/components/Checkbox.vue'
-
-    export default{
-     name: 'SwitchComponent',
-     components: {
-         Switch,
-         Template,
-         RadioButton,
-         ExpansionPanel,
-         Checkbox
-     },
-     data (){
-         return{
-
-         }
-     },
-        methods: {
-            ShowTitle(event){
-                
-            }
-        }
+export default {
+  data() {
+    return {
+      expanded: false
+    };
+  },
+  props: {
+    title: {
+      type: String,
+      required: true
     }
+  },
+  methods: {
+    togglePanel() {
+      this.expanded = !this.expanded;
+    }
+  }
+};
 </script>
 
 <style scoped>
-  .wrapperLeft{
-    width: 50%;
-    height: 100%;
-    padding: 20px;
-    padding-right: 10px;
-  }
+.expansion-panel {
+  border: 1px solid #7F5539;
+  border-radius: 4px;
+  margin-bottom: 10px;
+  min-width: 200px;
+}
 
-  .wrapperRight{
-    padding: 40px;
-    width: 50%;
-    height: 100%;
-    color: #7F5539;
-  }
+.panel-header {
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  background-color: #E6CCB2;
+  cursor: pointer;
+}
 
-  .attributes{
-    width: 100%;
-    min-height: 50px;
-    background: #e7c2a4;
-    border: 2px solid #7F5539;
-    border-radius: 20px;
-    margin: 10px 0 15px 0;
-    min-height: 15%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+.title {
+  flex-grow: 1;
+  font-weight: bold;
+  color: black;
+}
 
-  .expandTable{
-    width: 100%;
-  }
+.arrow-icon {
+  font-size: 14px;
+  transition: transform 0.3s;
+  color: black;
+  width: 20px;
+}
 
-  table{
-    width: 80%;
-  }
+.panel-content {
+  padding: 10px;
+  border-top: 1px solid black;
+  color: black;
+  background: #fbe5d0;
+}
 
-  .wrapperTop{
-    width: 100%;
-    height: 10%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
+.panel-transition-enter-active,
+.panel-transition-leave-active {
+  transition: height 0.1s ease;
+}
 
-  .wrapperBottom{
-    width: 100%;
-    height: 90%;
-    border: 2px solid #7F5539;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 30px;
-  }
-
-  .wrapperTop h1{
-    color: #7F5539;
-  }
+.panel-transition-enter,
+.panel-transition-leave-to {
+  height: 0;
+  overflow: hidden;
+}
 </style>
